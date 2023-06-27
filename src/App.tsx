f87ae3d1dcddca54bdb20a14be8957027448971b
Vijayakumar <vijayakumardpr@gmail.com>
2023-06-27 20:15:14 +0530

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { AuthRoutes } from "./pages/Auth/AuthRoutes";
 
 function App() {
   const host = window.location.host.split(".");
-  const hello = "Hello"
+  const hello = "vijayakumar"
   console.log(hello)
   if (host.length <= 3 && host[0] === "accounts") {
     return <AuthRoutes />;
