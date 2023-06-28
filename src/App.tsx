0ff746270e04e10bbef87e696b2b35d479101f5b
Priyangaj-17 <priyangaj17@gmai.com>
2023-06-27 22:24:11 -0700

    priyanga

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { AuthRoutes } from "./pages/Auth/AuthRoutes";
 
 function App() {
   const host = window.location.host.split(".");
-  const hello = "Hello";
+  const hello = "Hello world";
   console.log(hello);
   if (host.length <= 3 && host[0] === "accounts") {
     return <AuthRoutes />;
